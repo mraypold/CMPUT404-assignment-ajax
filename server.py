9566ad920368415ef4988bf2f03f6b665b3c0023
Michael Raypold <mraypold@users.noreply.github.com>
2015-02-28 08:37:12 -0700

    stepping through ajax

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2013 Abram Hindle
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,9 +22,14 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
-app = Flask(__name__)
+
+import os
+
+STATIC_DIR = os.path.abspath('static')
+
+app = Flask(__name__, static_url_path=STATIC_DIR)
 app.debug = True
 
 # An example world
@@ -36,7 +41,7 @@ app.debug = True
 class World:
     def __init__(self):
         self.clear()
-        
+
     def update(self, entity, key, value):
         entry = self.space.get(entity,dict())
         entry[key] = value
@@ -50,14 +55,14 @@ class World:
 
     def get(self, entity):
         return self.space.get(entity,dict())
-    
+
     def world(self):
         return self.space
 
 # you can test your webservice from the commandline
-# curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
+# curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}'
 
-myWorld = World()          
+myWorld = World()
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -74,26 +79,36 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return app.send_static_file('index.html')
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    print 'In Entity!'
+    return jsonify({'hello':'world'})
 
-@app.route("/world", methods=['POST','GET'])    
+@app.route("/world", methods=['POST','GET'])
 def world():
     '''you should probably return the world here'''
-    return None
+    print 'In World!'
+    print request.data
+    return jsonify({'hello':'world'})
 
-@app.route("/entity/<entity>")    
+@app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    print 'In Get Entity'
+    return jsonify({'hello':'world'})
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
+    if request.method == 'POST':
+        return redirect(url_for('hello'))
+
+    if request.method == 'GET':
+        return redirect(url_for('hello'))
+
     return None
 
 if __name__ == "__main__":
